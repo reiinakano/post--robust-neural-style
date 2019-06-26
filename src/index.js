6d3b95dbc692d98de1551239f5eb9c7e64ff3cfa
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2019-06-26 22:45:04 +0900

    rest of article (#1)
    * pw
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * moe

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import VisualTOC                      from './diagrams/VisualTOC.html';
 
+import ColabLink                      from './components/ColabLink.html';
+
 // eagerly initialize vtoc  as it's above the fold
 const tocNav = document.getElementById('vtoc');
 const visualTOC = new VisualTOC({target: tocNav});
@@ -11,3 +13,19 @@ const visualTOC = new VisualTOC({target: tocNav});
 //     const styleTransferExamples = new StyleTransferExamples({target: figure});
 //   });
 // }
+
+function addColabLink() {
+  const className = "add-colab-link";
+  const elements = document.getElementsByClassName(className);
+  for (const element of elements) {
+    const data = {
+      target: element, 
+      data: { 
+        url: "https://colab.research.google.com/github/reiinakano/adversarially-robust-neural-style-transfer/blob/master/Robust_Neural_Style_Transfer.ipynb" 
+      }
+    };
+    const colabLink = new ColabLink(data);
+  }
+}
+
+addColabLink();
